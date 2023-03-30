5a7fa17c0ab25ce7a6e3512f0ecf6570dae3c847
1um1n0517y01 <34325214+1um1n0517y01@users.noreply.github.com>
2023-03-30 21:44:26 +0200

    Transfered header into JSX; Added logo; Added css to React app

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+import './style.css';
+
 function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <header className='header'>
+      <div className='logo'>
+        <img src='logo.png' alt='Factify logo' width='68' height='68' />
+        <h1>Factify</h1>
+      </div>
+      <button className='btn btn-large btn-open'>Share a fact</button>
+    </header>
+  );
 }
 
 export default App;
