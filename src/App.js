33d2011aec68a01e3023177b2171f21d4d9dc1db
1um1n0517y01 <34325214+1um1n0517y01@users.noreply.github.com>
2023-04-01 14:11:01 +0200

    Uploading a Fact to Supabase

--- src/App.js
+++ src/App.js
@@ -136,28 +136,37 @@ function NewFactForm({ setFacts, setShowForm }) {
   const [text, setText] = useState('');
   const [source, setSource] = useState('http://example.com');
   const [category, setCategory] = useState('');
+  const [isUploading, setIsUploading] = useState(false);
   const textLength = text.length;
 
-  function handleSubmit(e) {
+  async function handleSubmit(e) {
     // 1. Prevent default
     e.preventDefault();
 
     // 2. Check if the data is valid, if so create a new fact
     if (text && isValidHttpUrl(source) && category && textLength >= 0) {
       // 3. Create a fact object
-      const newFact = {
-        id: Math.round(Math.random() * 10000000),
-        text,
-        source,
-        category,
-        votesInteresting: 0,
-        votesMindblowing: 0,
-        votesFalse: 0,
-        createdIn: new Date().getFullYear,
-      };
+      // const newFact = {
+      //   id: Math.round(Math.random() * 10000000),
+      //   text,
+      //   source,
+      //   category,
+      //   votesInteresting: 0,
+      //   votesMindblowing: 0,
+      //   votesFalse: 0,
+      //   createdIn: new Date().getFullYear,
+      // };
+
+      // 3. Uplaod fact to the Supabase and recive the new fact object
+      setIsUploading(true);
+      const { data: newFact, error } = await supabase
+        .from('facts')
+        .insert([{ text, source, category }])
+        .select();
+      setIsUploading(false);
 
       // 4. Add new fact to the user interface
-      setFacts((facts) => [newFact, ...facts]);
+      setFacts((facts) => [newFact[0], ...facts]);
 
       // 5. Reset the input fields
       setText('');
@@ -168,38 +177,46 @@ function NewFactForm({ setFacts, setShowForm }) {
       setShowForm(false);
     }
   }
-
-  return (
-    <form className='fact-form' onSubmit={handleSubmit}>
-      <input
-        type='text'
-        placeholder='Share a fact with the world...'
-        value={text}
-        onChange={(e) => setText(e.target.value)}
-      />
-      <span>{200 - textLength}</span>
-      <input
-        type='text'
-        placeholder='Trustworthy source...'
-        value={source}
-        onChange={(e) => setSource(e.target.value)}
-      />
-      <select
-        name=''
-        id=''
-        value={category}
-        onChange={(e) => setCategory(e.target.value)}
-      >
-        <option value=''>Choose Category:</option>
-        {CATEGORIES.map((cat) => (
-          <option key={cat.name} value={cat.name}>
-            {cat.name.toUpperCase()}
-          </option>
-        ))}
-      </select>
-      <button className='btn btn-large'>Post</button>
-    </form>
-  );
+  if (!isUploading) {
+    return (
+      <form className='fact-form' onSubmit={handleSubmit}>
+        <input
+          type='text'
+          placeholder='Share a fact with the world...'
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+          disabled={isUploading}
+        />
+        <span>{200 - textLength}</span>
+        <input
+          type='text'
+          placeholder='Trustworthy source...'
+          value={source}
+          onChange={(e) => setSource(e.target.value)}
+          disabled={isUploading}
+        />
+        <select
+          name=''
+          id=''
+          value={category}
+          onChange={(e) => setCategory(e.target.value)}
+          disabled={isUploading}
+        >
+          <option value=''>Choose Category:</option>
+          {CATEGORIES.map((cat) => (
+            <option key={cat.name} value={cat.name}>
+              {cat.name.toUpperCase()}
+            </option>
+          ))}
+        </select>
+        <button className='btn btn-large' disabled={isUploading}>
+          Post
+        </button>
+      </form>
+    );
+  } else {
+    return <p className='message'>Uploading fact...</p>;
+  }
 }
 
 function CategoryFilter({ setCurrentCategory }) {
